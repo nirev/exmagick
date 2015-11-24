8606ccd1d84d482532d5450cdfd85a8dfeece82c
dgvncsz0f <dsouza@c0d3.xxx>
2015-11-24 20:47:40 -0200

    ability to set ImageInfo adjoin flag
    This flag changes the behavior of `load_image' and `write_image' when
    the image has multiple frames. For instance, on `write_image` it can be
    used to create multiple images, one for each frame.
    
    The image_dump image has been rewritten to use `WriteImages` instead of
    `WriteImage`. This functions does the same thing as `WriteImage` but can
    use the `adjoin` flag.

--- lib/c/exmagick.c
+++ lib/c/exmagick.c
@@ -18,6 +18,8 @@
 #include <string.h>
 #include <langinfo.h>
 
+#define EXM_MAX_ATOM_SIZE 10
+#define EXM_INIT char *errmsg = NULL
 #define EXM_FAIL(j, m) do { errmsg = m; goto j; } while (0)
 
 typedef struct {
@@ -31,16 +33,21 @@ static void exmagick_unload (ErlNifEnv *env, void *data);
 static void exmagick_destroy (ErlNifEnv *env, void *data);
 static char *exmagick_utf8strcpy(char *dst, ErlNifBinary *utf8, size_t len);
 static int exmagick_get_utf8str (ErlNifEnv *env, ERL_NIF_TERM arg, ErlNifBinary *utf8);
+static int exmagick_get_boolean_u (ErlNifEnv *env, ERL_NIF_TERM arg, unsigned int *p);
+
 static ERL_NIF_TERM exmagick_make_utf8str (ErlNifEnv *env, const char *data);
 static ERL_NIF_TERM exmagick_image (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM exmagick_image_load (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM exmagick_image_dump (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 
+static ERL_NIF_TERM exmagick_info_set_opt(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
+
 ErlNifFunc exmagick_interface[] =
 {
   {"image", 0, exmagick_image},
   {"image_load", 2, exmagick_image_load},
-  {"image_dump", 2, exmagick_image_dump}
+  {"image_dump", 2, exmagick_image_dump},
+  {"info_set_opt", 3, exmagick_info_set_opt}
 };
 
 ERL_NIF_INIT(Elixir.ExMagick, exmagick_interface, exmagick_load, NULL, NULL, exmagick_unload)
@@ -54,6 +61,24 @@ int exmagick_load (ErlNifEnv *env, void **data, const ERL_NIF_TERM info)
   InitializeMagick(NULL);
   *data = type;
   return(0);
+}
+
+static
+int exmagick_get_boolean_u (ErlNifEnv *env, ERL_NIF_TERM arg, unsigned int *p)
+{
+  int ecode;
+  char atom[EXM_MAX_ATOM_SIZE];
+  if (0 != (ecode = enif_get_atom(env, arg, atom, EXM_MAX_ATOM_SIZE, ERL_NIF_LATIN1)))
+  {
+    if (strcmp("true", atom) == 0)
+    { *p = 1; }
+    else if (strcmp("false", atom) == 0)
+    { *p = 0; }
+    else
+    { ecode = 0; }
+  }
+
+  return(ecode);
 }
 
 static
@@ -101,7 +126,7 @@ ERL_NIF_TERM exmagick_image (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]
 {
   ERL_NIF_TERM result;
 
-  char *errmsg             = NULL;
+  EXM_INIT;
   ErlNifResourceType *type = (ErlNifResourceType *) enif_priv_data(env);
   exm_resource_t *resource = enif_alloc_resource(type, sizeof(exm_resource_t));
   if (resource == NULL)
@@ -128,12 +153,39 @@ ehandler:
 }
 
 static
+ERL_NIF_TERM exmagick_info_set_opt (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+  char atom[EXM_MAX_ATOM_SIZE];
+  exm_resource_t *resource;
+
+  EXM_INIT;
+  ErlNifResourceType *type = (ErlNifResourceType *) enif_priv_data(env);
+
+  if (0 == enif_get_resource(env, argv[0], type, (void **) &resource))
+  { EXM_FAIL(ehandler, "argv[0]: bad argument"); }
+
+  if (0 == enif_get_atom(env, argv[1], atom, EXM_MAX_ATOM_SIZE, ERL_NIF_LATIN1))
+  { EXM_FAIL(ehandler, "argv[1]: bad argument"); }
+
+  if (strcmp("adjoin", atom) == 0)
+  {
+    if (0 == exmagick_get_boolean_u(env, argv[2], &resource->i_info->adjoin))
+    { printf("wat\n"); EXM_FAIL(ehandler, "argv[2]: bad argument"); }
+  }
+
+  return(enif_make_tuple2(env, enif_make_atom(env, "ok"), argv[0]));
+
+ehandler:
+  return(enif_make_tuple2(env, enif_make_atom(env, "error"), exmagick_make_utf8str(env, errmsg)));
+}
+
+static
 ERL_NIF_TERM exmagick_image_load (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
   ErlNifBinary utf8;
   exm_resource_t *resource;
 
-  char *errmsg             = NULL;
+  EXM_INIT;
   ErlNifResourceType *type = (ErlNifResourceType *) enif_priv_data(env);
 
   if (0 == enif_get_resource(env, argv[0], type, (void **) &resource))
@@ -156,7 +208,7 @@ ERL_NIF_TERM exmagick_image_load (ErlNifEnv *env, int argc, const ERL_NIF_TERM a
     EXM_FAIL(ehandler, resource->e_info.reason);
   }
   
-  return(enif_make_atom(env, "ok"));
+  return(enif_make_tuple2(env, enif_make_atom(env, "ok"), argv[0]));
 
 ehandler:
   return(enif_make_tuple2(env, enif_make_atom(env, "error"), exmagick_make_utf8str(env, errmsg)));
@@ -165,10 +217,11 @@ ehandler:
 static
 ERL_NIF_TERM exmagick_image_dump (ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
+  char filename[MaxTextExtent];
   ErlNifBinary utf8;
   exm_resource_t *resource;
 
-  char *errmsg             = NULL;
+  EXM_INIT;
   ErlNifResourceType *type = (ErlNifResourceType *) enif_priv_data(env);
 
   if (0 == enif_get_resource(env, argv[0], type, (void **) &resource))
@@ -177,14 +230,14 @@ ERL_NIF_TERM exmagick_image_dump (ErlNifEnv *env, int argc, const ERL_NIF_TERM a
   if (0 == exmagick_get_utf8str(env, argv[1], &utf8))
   { EXM_FAIL(ehandler, "argv[1]: bad argument"); }
 
-  exmagick_utf8strcpy(resource->image->filename, &utf8, MaxTextExtent);
-  if (0 == WriteImage(resource->i_info, resource->image))
+  exmagick_utf8strcpy(filename, &utf8, MaxTextExtent);
+  if (0 == WriteImages(resource->i_info, resource->image, filename, &resource->e_info))
   {
-    CatchException(&resource->image->exception);
-    EXM_FAIL(ehandler, resource->image->exception.reason);
+    CatchException(&resource->e_info);
+    EXM_FAIL(ehandler, resource->e_info.reason);
   }
 
-  return(enif_make_atom(env, "ok"));
+  return(enif_make_tuple2(env, enif_make_atom(env, "ok"), argv[0]));
 
 ehandler:
   return(enif_make_tuple2(env, enif_make_atom(env, "error"), exmagick_make_utf8str(env, errmsg)));
