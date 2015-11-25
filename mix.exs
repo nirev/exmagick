4838e448198e2c7e4474e7e706c05c57af8f1e5c
dgvncsz0f <dsouza@c0d3.xxx>
2015-11-25 01:27:02 -0200

    add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -28,7 +28,10 @@ defmodule ExMagick.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
   end
 
   defp aliases do
