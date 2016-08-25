0bb0402562dc0cdc7532e3bd6c6f8ac27c76f422
nirev <guilherme@nirev.org>
2016-08-25 16:07:39 -0300

    bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ExMagick.Mixfile do
 
   def project do
     [ app: :exmagick,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.2",
       description: description,
       package: package,
